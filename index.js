2803fbed3b5340461ca8c6651f33018b2e1f3102
Valeria Leškina <valeria.leskina@tptlive.ee>
2025-09-22 15:22:21 +0300

    let example

---
+++ index.js
@@ -0,0 +1,36 @@
+let num = 1
+console.log(num);
+console.log(typeof num);
+num = 1.2;
+console.log(num);
+console.log(typeof num);
+let text = 'Hello world';
+console.log(text);
+console.log(typeof text);
+text = 'H'; //char
+console.log(text);
+console.log(typeof text);
+let bool = true;
+console.log(bool);
+console.log(typeof bool);
+bool = false;
+console.log(bool);
+console.log(typeof bool);
+let undef = undefined;
+console.log(undef);
+console.log(typeof undef);
+let nil = null;
+console.log(nil);
+console.log(typeof nil);
+let array = [1, 2 'hello', 'world', true, [1, 2, 3]];
+console.log(array);
+console.log(typeof arrar);
+let obj = {
+    name: 'Valeria',
+    age: 17,
+    hairColor: 'brown',
+    isFemale: true,
+};
+console.log(obj);
+console.log(typeof obj);
+

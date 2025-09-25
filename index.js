abc45862cbe6f883b5f8befa09ab7098e40ba6ac
Valeria Leškina <valeria.leskina@tptlive.ee>
2025-09-25 15:52:34 +0300

    number example

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
-let bool = true;
-console.log(bool);
-bool =false;
-console.log(bool);
-bool = true && false; // AND
-bool =false && true;
-bool =false && false;
-bool = true && true;
-console.log(bool);
-bool = true || false; // OR
-bool = true || false; // OR
-bool = true || false; // OR
-bool = true || false; // OR
-console.log(bool);
-bool = !true; //NOT
-console.log(bool);
-bool = !true; //NOT
-console.log(bool);
+let num = 10;
+if (num > 10) {
+    console.log('bigger than 10');
+} else if (num === 10){
+    console.log('equal to 10');
+}
+else {
+    console.log('smaller than 10');
+}
  
-bool = true;
-bool= ( true && false || true) && !( false || true && false);
-console.log(bool);
+let day = new Date("2025-09-28").getDay();
+console.log(day);
  
-bool = 10 > 5;
-bool = 10 < 5;
-bool = 10 > 10;
-bool = 10 < 10;
-bool = 10 == 10;
-bool = '10' == 10;
-bool = '10' === 10; // strict will check if types match before values
-bool = 10 >= 10;
-bool = 10 <= 10;
-bool = '10' != 10;
-bool = '10' !== 10;
-bool = [] == '';
-bool = [1, 2, 3] == 1;
-console.log(bool);
-
+if(day === 1) {
+console.log('Esmaspäev');
+} else if (day === 2) {
+console.log('Teisipäev');
+} else if (day === 3) {
+console.log('Kolmapäev');
+} else if (day === 4) {
+console.log('Neljapäev');
+} else if (day === 5) {
+console.log('Reede');
+} else if (day === 6) {
+console.log('Laupäev');
+} else if (day === 0) {
+console.log('Pühapäev');
+} else {
+console.log('Imelikpäev');
+}
  
  

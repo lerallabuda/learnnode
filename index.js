59b08fe5668c74bba9d1c9dd569a8e080b65470a
Valeria Leškina <valeria.leskina@tptlive.ee>
2025-09-25 14:34:03 +0300

    Add string method example

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
-let num = 1
-console.log(num);
-console.log(typeof num);
-num = 1.2;
-console.log(num);
-console.log(typeof num);
-let text = 'Hello world';
+let text = 'Hello';
+text = text + 'Valeria';
 console.log(text);
-console.log(typeof text);
-text = 'H'; //char
+text = 'asdast';
+text = "asdadsasd";
+text = "Don't use double quotes unless needed";
+text = 'Don\'t use double\t quotes\n unless needed';
 console.log(text);
-console.log(typeof text);
-let bool = true;
-console.log(bool);
-console.log(typeof bool);
-bool = false;
-console.log(bool);
-console.log(typeof bool);
-let undef = undefined;
-console.log(undef);
-console.log(typeof undef);
-let nil = null;
-console.log(nil);
-console.log(typeof nil);
-let array = [1, 2, 'hello', 'world', true, [1, 2, 3]];
-console.log(array);
-console.log(typeof arrar);
-let obj = {
-    name: 'Valeria',
-    age: 17,
-    hairColor: 'brown',
-    isFemale: true,
-};
-console.log(obj);
-console.log(typeof obj);
+let name = 'Valeria';
+let age = 17;
+let sexuality = 'BI';
+text = "your name is" + name + 'and you are' + age + 'years old and your sexuslity is' + sexuality + '!';
+console.log(text);
+text = `your name is ${name}
+and you are ${age} years old 
+and your sexuality is ${sexuality}!`;
+console.log(text);
+console.log(text.toUpperCase());
+
+text = text.toUpperCase();
+
+console.log(text);
+ 
 
